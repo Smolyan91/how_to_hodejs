5c8fa50c1579346741d43c6b6ce09ef9443f6601
Smolyanov Igor <smolyan_91@mail.ru>
2021-01-05 23:06:54 +0300

    editor

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,10 +1,32 @@
-import * as React from 'react'
+import * as React from "react";
+import { TAB } from "../../constants";
+import "./Editor.css";
 
+const TAB_LENGTH = 4;
 
-const Editor = () => {
-    return (
-        <div className='editor'></div>
-    )
-}
+export const Editor = () => {
+  const [value, onSetValue] = React.useState<string>("");
+
+  const onKeyDown = React.useCallback((event) => {
+    if (event.keyCode !== TAB) return;
+
+    event.preventDefault();
+    const { currentTarget: textarea } = event;
+    const { selectionStart: s0, selectionEnd: s1, value } = textarea;
+    const newValue = value.substring(0, s0) + "    " + value.substring(s1);
+    textarea.setSelectionRange(s0 + TAB_LENGTH, s1 + TAB_LENGTH);
+    onSetValue(newValue);
+  }, []);
+
+  return (
+    <textarea
+      className="editor"
+      value={value}
+      placeholder="Your code"
+      onChange={(event) => onSetValue(event.currentTarget.value)}
+      onKeyDown={onKeyDown}
+    ></textarea>
+  );
+};
 
 export default Editor;
